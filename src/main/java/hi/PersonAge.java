1a8ecb67161488eb2c96cf43772a15673729277b
joega <joega@10.84.8.237>
2026-02-05 19:30:26 -0500

    Committing prep work for CSC 360.

---
+++ src/main/java/hi/PersonAge.java
@@ -0,0 +1,87 @@
+package hi;
+
+public class PersonAge
+{
+	String firstName;
+	String lastName;
+	int age;
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param age
+	 */
+	public PersonAge(String firstName, String lastName, int age)
+	{
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName()
+	{
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge()
+	{
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	@Override
+	public String toString()
+	{
+		return "Person: " + firstName + " " + lastName + ", " + age;
+	}
+	
+	public PersonAge makePerson(PersonAge that) throws UnderAgeException 
+	{
+		if(age<18 || that.age<18)
+		{
+			throw new UnderAgeException();
+		}
+		PersonAge child = new PersonAge(this.firstName, that.lastName, 0);
+		return child;
+	}
+	
+	public static void main(String[] args) {
+		PersonAge r = new PersonAge("Romeo","Mon",15);
+		
+		System.out.println("R "+r);
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/hi/UnderAgeException.java
@@ -0,0 +1,26 @@
+package hi;
+
+public class UnderAgeException extends Exception
+{
+	private static final long serialVersionUID = 4554479932935169519L;
+
+	/**
+	 * 
+	 */
+	public UnderAgeException()
+	{
+		super("Parent is too young");
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public UnderAgeException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
