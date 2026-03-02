d0089c6756d076ecbd0b640a1553aa78d92de082
joega <joega@10.84.8.237>
2026-03-02 10:28:55 -0500

    minimal changes

---
+++ src/main/java/hi/PasswordServer.java
@@ -0,0 +1,12 @@
+package hi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/")
+public class PasswordServer
+{
+	
+}
