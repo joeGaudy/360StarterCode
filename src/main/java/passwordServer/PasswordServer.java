2623ac646e611ded3baa121033adb5c76f443ffa
joega <joega@10.84.8.237>
2026-03-06 10:28:18 -0500

    Password server finished.

--- src/main/java/passwordServer/PasswordServer.java
+++ src/main/java/passwordServer/PasswordServer.java
@@ -1,12 +1,83 @@
 package passwordServer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/")
 public class PasswordServer
 {
+	Map<String, String> passwords = new HashMap<>();
+	List<String> authenticated = new ArrayList<>();
+	List<String> requested = new ArrayList<>();
+	
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping("/")
+	public String homePage() {
+	    return "<html><body>" +
+	        "<h1>Password Server</h1>" +
+	        "<ol>" +
+	        "<li><a href='request'>request/</a> Users that have requested a password.</li>" +
+	        "<li><a href='auth'>auth/</a> Users that have authenticated their password</li>" +
+	        "<li><a href='request/joegaudy'>request/:username</a> Request for a password with your username.</li>" +
+	        "<li><a href='auth/joegaudy/2432'>auth/:username/:password</a> Authenticates the password for your username.</li>" +
+	        "</ol>" +
+	        "</body></html>";
+	}
+	
+	@ResponseStatus(HttpStatus.OK)
+    @GetMapping("/request")
+    public String requestedUsers() {
+		return "Requesters: " + requested;
+    }
+	
+	@ResponseStatus(HttpStatus.OK)
+    @GetMapping("/auth")
+    public String authenticatedUsers() {
+		return "Authenticated: " + authenticated;
+    }
+	
+	@ResponseStatus(HttpStatus.OK)
+    @GetMapping("/request/{username}")
+    public String requestPassword(@PathVariable String username) {
+		int password = Math.abs(username.hashCode()) % 10000;
+		passwords.put(username, String.valueOf(password));
+		
+		if (!requested.contains(username)) {
+	        requested.add(username);
+	    }
+		
+		return String.valueOf(password);
+    }
+	
+	@ResponseStatus(HttpStatus.OK)
+    @GetMapping("/auth/{username}/{password}")
+    public String authenticatePassword(@PathVariable String username, @PathVariable String password) {
+		
+		if (!passwords.containsKey(username)) {
+		    return "Username not found";
+		}
+		
+		if (passwords.get(username).equals(password)) {
+			
+			if (!authenticated.contains(username)) {
+		        authenticated.add(username);
+		    }
+			return "Authentication Successful";
+		}
+		else {
+			return "Authentication Failed";
+		}
+    }
 	
 }
---
+++ src/main/java/passwordServer/SpringBootPasswordServer.java
@@ -0,0 +1,13 @@
+package passwordServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPasswordServer
+{
+	public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootPasswordServer.class, args);
+    }
+}
